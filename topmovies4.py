8ac9c267cd4d06489310c0e9f9af4f0f7e4e2066
Valiant Dmello <49265710+ValiantDmello@users.noreply.github.com>
2023-02-26 21:28:49 -0500

    first commit

--- topmovies4.py
+++ topmovies4.py
@@ -1,6 +1,8 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
 class RatingsBreakdown(MRJob):
     def steps(self):
-        def steps(self):
         return [
             MRStep(mapper=self.mapper,
                    reducer=self.reducer1),
