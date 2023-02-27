61fe5bb1c9e3e8ae5bb06eade4feb6783227a9f4
Valiant Dmello <49265710+ValiantDmello@users.noreply.github.com>
2023-02-26 20:38:57 -0500

    first commit

--- topmovies.py
+++ topmovies.py
@@ -9,14 +9,14 @@ class RatingsBreakdown(MRJob):
             MRStep(reducer=self.reducer2)
         ]
 
-    def mapper(self, _, line):  //mapper function
+    def mapper(self, _, line):  #mapper function
         (userID, movieID, rating, timestamp) = line.split('\t')
         yield movieID, 1
 
-    def reducer1(self, key, values):   //reducer to get ratings count
+    def reducer1(self, key, values):   #reducer to get ratings count
         yield str(sum(values)).zfill(5), key
 
-    def reducer2(self, count, movies): //reducer to sort as per most ratings
+    def reducer2(self, count, movies): #reducer to sort as per most ratings
         for movie in movies:
             yield movie, count
 
